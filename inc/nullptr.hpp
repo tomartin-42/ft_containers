4d037f0f973f028d2085209f0bf05916d1e8826a
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-03-02 12:51:15 +0100

    add nullptr_t

---
+++ inc/nullptr.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nullptr.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 12:13:30 by tomartin          #+#    #+#             */
+/*   Updated: 2022/03/02 12:49:05 by tomartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __NULLPTR__HPP
+# define __NULLPTR__HPP
+
+namespace ft
+{
+
+static class nullptr_t
+{
+	public:
+	template<class T>
+	operator T*() const { return (0); }
+
+	template<class C, class T>
+	operator T C::*() const { return (0); }
+
+	private:
+		void operator&() const;
+
+} nullptr_t = {};
+
+} //end ft
+#endif
--- inc/vector.hpp
+++ inc/vector.hpp
@@ -1,26 +1,39 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+#include "nullptr.hpp"
+
 template <class T, class alloc = allocator<T>i > 
 class vector
 {
 	public:
-		typedef	alloc											allocator_type;
-		typedef T												value_type;
-		typedef typename alloc_type::reference					reference;
-		typedef typename alloc_type::const_eference				const_reference;
-		typedef typename alloc_type::pointer					pointer;
-		typedef typename alloc_type::const_pointer				const_pointer;
-		typedef std::random_access_iterator<value_type>			iterator;
-		typedef std::random_access_iterator<const value_type>	const_iterator;
-		typedef std::random_access_iterator<iterator>			reverse_iterator;
-		typedef std::random_access_iterator<const iterator>		const_reverse_iterator;
+		typedef	alloc														allocator_type;
+		typedef T															value_type;
+		typedef typename alloc_type::reference								reference;
+		typedef typename alloc_type::const_eference							const_reference;
+		typedef typename alloc_type::pointer								pointer;
+		typedef typename alloc_type::const_pointer							const_pointer;
+		typedef std::random_access_iterator<value_type>						iterator;
+		typedef std::random_access_iterator<const value_type>				const_iterator;
+		typedef std::random_access_iterator<iterator>						reverse_iterator;
+		typedef std::random_access_iterator<const iterator>					const_reverse_iterator;
+		typedef typename std::iterator_traits<iterator>::difference_type 	difference_type;
+		typedef typename allocator_type::size_type							size_type;
+
+	private:
+		pointer     						_begin;
+		pointer     						_end;
+		size_type   						_size;
+		std::pair<pointer, allocator_type> 	_end_cap;
 		
-/*		//constructors
+		//constructors
 		//empty
 		explicit vector (const allocator_type& alloc = allocator_type()) 
+			: _begin(ft::nullptr_t), _end(_begin), _size(0)
+		{
+		}
 
-		//fill
+		//fill 
 		explicit vector (size_type n, const value_type& val = value_type(),
                  const allocator_type& alloc = allocator_type())
 
@@ -32,4 +45,3 @@ class vector
 		vector (const vector& x)
 */
 
-		
