043cb093d9747f73092f846f0bb20f25ef6fceb7
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-03-19 18:40:48 +0100

    work in vector

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 19:41:05 by tomartin          #+#    #+#             */
-/*   Updated: 2022/03/12 20:09:51 by tomartin         ###   ########.fr       */
+/*   Updated: 2022/03/19 18:40:24 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,47 @@
 # define VECTOR_HPP
 
 #include "nullptr.hpp"
+#include <memory>
 
 namespace ft
 {
-	template <class T, class alloc = allocator<T> > 
+	template <class T, class alloc = std::allocator<T> > 
 	class vector
 	{
 		public:
 			typedef T															value_type;
-			typedef	alloc														allocator_type;
+			typedef	alloc														alloc_type;
 			typedef typename alloc_type::reference								reference;
 			typedef typename alloc_type::const_eference							const_reference;
 			typedef typename alloc_type::pointer								pointer;
 			typedef typename alloc_type::const_pointer							const_pointer;
-			typedef std::random_access_iterator<value_type>						iterator;
-			typedef std::random_access_iterator<const value_type>				const_iterator;
+			typedef std::iterator<std::random_access_iterator_tag, value_type>		iterator;
+			typedef std::iterator<std::random_access_iterator_tag, const value_type>	const_iterator;
 			typedef std::reverse_iterator<iterator>								reverse_iterator;
 			typedef std::reverse_iterator<const iterator>						const_reverse_iterator;
+			//typedef std::random_access_iterator<value_type>						iterator;
+			//typedef std::random_access_iterator<const value_type>				const_iterator;
+			//typedef std::reverse_iterator<iterator>								reverse_iterator;
+			//typedef std::reverse_iterator<const iterator>						const_reverse_iterator;
 			typedef typename std::iterator_traits<iterator>::difference_type 	difference_type;
-			typedef typename allocator_type::size_type							size_type;
+			typedef typename alloc_type::size_type								size_type;
 	
 		private:
-			allocator_type						_alloc;
-			pointer     						_start;
-			pointer     						_end;
-			pointer								_end_capacity;
+			alloc_type							_alloc;			// data type //
+			pointer     						_start;			// where init data //
+			pointer     						_end;			// where init empty data //
+			pointer								_end_capacity;	// where end the prealocate data //
+			size_type							_size;			// number of alocate data //
 			
 			//constructors==============================================================================
 
 			//empty
-			explicit vector (const allocator_type& alloc = allocator_type()) 
-				: _begin(ft::nullptr_t), _end(_begin), _size(0)
-			{
-			}
+			explicit vector (const alloc_type& alloc = alloc_type()) 
+				: _start(ft::nullptr_t), _end(_begin), _size(0)
+			{}
 	
 			//fill 
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+			explicit vector (size_type n, const value_type& val = value_type(), const alloc_type& alloc = alloc_type())
 				: _start(ft::nullptr_t), _end(_start), _size(n)
 			{
 				this->_start = _alloc.allocate(n);
@@ -63,15 +68,13 @@ namespace ft
 	
 			//range
 	/*		template <class InputIterator>
-	        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+	        vector (InputIterator first, InputIterator last, const alloc_type& alloc = alloc_type())
 			{
 	
 	
 			//copy
 			vector (const vector& x)
 	*/
-	};
+	};// end vector class
 } // end namespace ft
 #endif
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/25 11:19:06 by tomartin          #+#    #+#             */
-/*   Updated: 2022/02/28 11:46:17 by tomartin         ###   ########.fr       */
+/*   Created: 2022/03/19 18:23:12 by tomartin          #+#    #+#             */
+/*   Updated: 2022/03/19 18:24:50 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stack.hpp"
-#include <iostream>
+#include "vector.hpp"
 
-int main(void)
+int	main (void)
 {
-	stack<int>	num;
+	ft::vector<int>	i;
 
-	std::cout << "empy = " << num.empty() << std::endl;
-
-	for(int i = 0; i < 10; i++)
-	{
-		num.push(i);
-		std::cout << "top = " << num.top() << std::endl;
-	}
-	std::cout << "size = " << num.size() << std::endl;
-	std::cout << "top = " << num.top() << std::endl;
-	num.pop();
-	std::cout << "POP" << std::endl;
-	std::cout << "size = " << num.size() << std::endl;
-	std::cout << "top = " << num.top() << std::endl;
-	stack<int> other(num);
-	for (; !other.empty(); )
-	{
-		std::cout << "other.top = " << other.top() << std::endl;
-		other.pop();
-	}
+	return 0;
 }
+
