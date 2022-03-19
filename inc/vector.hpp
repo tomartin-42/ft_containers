861adb143cce564b52b0da3c9141db71cd1dabec
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-03-19 18:47:06 +0100

    work in vector, first main

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 19:41:05 by tomartin          #+#    #+#             */
-/*   Updated: 2022/03/19 18:40:24 by tomartin         ###   ########.fr       */
+/*   Updated: 2022/03/19 18:46:31 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ namespace ft
 			typedef T															value_type;
 			typedef	alloc														alloc_type;
 			typedef typename alloc_type::reference								reference;
-			typedef typename alloc_type::const_eference							const_reference;
+			//typedef typename alloc_type::const_eference							const_reference;
 			typedef typename alloc_type::pointer								pointer;
 			typedef typename alloc_type::const_pointer							const_pointer;
 			typedef std::iterator<std::random_access_iterator_tag, value_type>		iterator;
 			typedef std::iterator<std::random_access_iterator_tag, const value_type>	const_iterator;
 			typedef std::reverse_iterator<iterator>								reverse_iterator;
 			typedef std::reverse_iterator<const iterator>						const_reverse_iterator;
-			//typedef std::random_access_iterator<value_type>						iterator;
-			//typedef std::random_access_iterator<const value_type>				const_iterator;
-			//typedef std::reverse_iterator<iterator>								reverse_iterator;
-			//typedef std::reverse_iterator<const iterator>						const_reverse_iterator;
+			//typedef ft::random_access_iterator<value_type>					iterator;
+			//typedef ft::random_access_iterator<const value_type>				const_iterator;
+			//typedef ft::reverse_iterator<iterator>							reverse_iterator;
+			//typedef ft::reverse_iterator<const iterator>						const_reverse_iterator;
 			typedef typename std::iterator_traits<iterator>::difference_type 	difference_type;
 			typedef typename alloc_type::size_type								size_type;
 	
@@ -47,14 +47,15 @@ namespace ft
 			size_type							_size;			// number of alocate data //
 			
 			//constructors==============================================================================
-
+		public:
 			//empty
-			explicit vector (const alloc_type& alloc = alloc_type()) 
-				: _start(ft::nullptr_t), _end(_begin), _size(0)
+			//explicit vector (const alloc_type& alloc = alloc_type()) 
+			explicit vector () 
+				: _start(ft::nullptr_t), _end(_start), _size(0)
 			{}
 	
 			//fill 
-			explicit vector (size_type n, const value_type& val = value_type(), const alloc_type& alloc = alloc_type())
+			explicit vector (size_type n, const value_type& val = value_type(), const alloc_type& _alloc = alloc_type())
 				: _start(ft::nullptr_t), _end(_start), _size(n)
 			{
 				this->_start = _alloc.allocate(n);
