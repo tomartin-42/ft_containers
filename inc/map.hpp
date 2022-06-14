1874c2c3749531c5babaf2824cbeea285778fe57
tommy <tommy@localhost.localdomain>
2022-06-14 11:43:42 +0200

    init map

---
+++ inc/map.hpp
@@ -0,0 +1,13 @@
+#include <memroy> // to allocate
+#include <iostream>
+
+namespace ft
+{
+	template<class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key, T> > > 
+	class map
+	{
+
+	}; //end map class
+}//end namespace ft
+
+	
